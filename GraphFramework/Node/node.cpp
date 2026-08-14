14aa6c2b25cdd9da30d89c639e7c3fe83d99b4fb
AntonioVisan <antonio.visan@student.unitbv.ro>
2026-08-14 18:56:21 +0300

    refactor(node): improve node class implementation

--- GraphFramework/Node/node.cpp
+++ GraphFramework/Node/node.cpp
@@ -2,35 +2,34 @@
 
 Node::Node(int index, QPoint p) : m_index(index), m_coord(p)
 {
-
 }
 
 void Node::setIndex(int index)
 {
-    m_index=index;
+    m_index = index;
 }
 
-void Node::setCoord(QPoint p)
+void Node::setCoord(const QPoint& p)
 {
-    m_coord=p;
+    m_coord = p;
 }
 
-int Node::getIndex()
+int Node::getIndex() const
 {
     return m_index;
 }
 
-QPoint Node::getCoord()
+QPoint Node::getCoord() const
 {
     return m_coord;
 }
 
-int Node::getX()
+int Node::getX() const
 {
     return m_coord.x();
 }
 
-int Node::getY()
+int Node::getY() const
 {
     return m_coord.y();
 }
--- GraphFramework/Node/node.h
+++ GraphFramework/Node/node.h
@@ -1,18 +1,23 @@
 #ifndef NODE_H
 #define NODE_H
-#include <Qpoint>
+
+#include <QPoint>
+
 class Node
 {
     int m_index;
     QPoint m_coord;
+
 public:
-    Node(int index=0, QPoint p = QPoint(0,0));
+    Node(int index = 0, QPoint p = QPoint(0,0));
+
     void setIndex(int index);
-    void setCoord(QPoint p);
-    int getIndex();
-    QPoint getCoord();
-    int getX();
-    int getY();
+    void setCoord(const QPoint& p);
+
+    int getIndex() const;
+    QPoint getCoord() const;
+    int getX() const;
+    int getY() const;
 };
 
 #endif
