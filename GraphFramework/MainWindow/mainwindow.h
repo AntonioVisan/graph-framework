78c46ed3a59110ea2489501177687a727175c255
AntonioVisan <antonio.visan@student.unitbv.ro>
2026-08-14 19:33:59 +0300

    refactor(main-window): simplify graph interaction logic
    - remove redundant edge duplication checks from MainWindow
    - use const references/pointers where objects are not modified
    - improve code formatting and consistency
    - keep node creation, selection, movement and edge drawing behavior

--- GraphFramework/MainWindow/mainwindow.h
+++ GraphFramework/MainWindow/mainwindow.h
@@ -4,35 +4,41 @@
 #include <QMainWindow>
 #include <QMouseEvent>
 #include <QPaintEvent>
+#include <QPainter>
+
 #include "../Graph/graph.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+	class MainWindow;
 }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
+
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	MainWindow(QWidget* parent = nullptr);
+	~MainWindow();
 
-    void mouseReleaseEvent(QMouseEvent *m) override;
-    void paintEvent(QPaintEvent *e) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mousePressEvent(QMouseEvent *event) override;
+	void mouseReleaseEvent(QMouseEvent* event) override;
+	void paintEvent(QPaintEvent* event) override;
+	void mouseMoveEvent(QMouseEvent* event) override;
+	void mousePressEvent(QMouseEvent* event) override;
+
 private slots:
-
-    void drawArrow(QPainter &p, QPoint start, QPoint end);
-    void on_radioButton_toggled(bool checked);
-
-    void on_radioButton_2_toggled(bool checked);
+	void on_radioButton_toggled(bool checked);
+	void on_radioButton_2_toggled(bool checked);
 
 private:
-    Ui::MainWindow *ui;
-    Graph m_graph;
-    Node* m_firstNode=nullptr;
-    Node* m_pressedNode=nullptr;
+	void drawArrow(QPainter& painter, QPoint start, QPoint end);
+
+	Ui::MainWindow* ui;
+	Graph m_graph;
+
+	Node* m_firstNode = nullptr;
+	Node* m_pressedNode = nullptr;
 };
+
 #endif
