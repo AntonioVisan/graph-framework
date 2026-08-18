c56056c0d9aed298e8afb871ff68fa62873babc3
AntonioVisan <antonio.visan@student.unitbv.ro>
2026-08-18 20:53:59 +0300

    feat(node): extend node with name support
    - add node name field
    - add name getter and setter
    - initialize node name
    - preserve existing graph, labyrinth and map functionality

--- GraphFramework/Node/node.cpp
+++ GraphFramework/Node/node.cpp
@@ -1,103 +1,114 @@
 #include "../Node/node.h"
 
 Node::Node(int index, QPoint p)
-    : m_index(index),
-    m_coord(p),
-    m_row(0),
-    m_column(0),
-    m_color(QColor()),
-    m_longitude(0.0),
-    m_latitude(0.0)
+	: m_index(index),
+	m_coord(p),
+	m_row(0),
+	m_column(0),
+	m_color(QColor()),
+	m_longitude(0.0),
+	m_latitude(0.0),
+	m_name("")
 {}
 
 void Node::setIndex(int index)
 {
-    m_index = index;
+	m_index = index;
 }
 
 void Node::setCoord(const QPoint& p)
 {
-    m_coord = p;
+	m_coord = p;
 }
 
 void Node::setGridPos(int row, int column)
 {
-    m_row = row;
-    m_column = column;
+	m_row = row;
+	m_column = column;
 }
 
 void Node::setRow(int row)
 {
-    m_row = row;
+	m_row = row;
 }
 
 void Node::setColumn(int column)
 {
-    m_column = column;
+	m_column = column;
 }
 
 void Node::setColor(const QColor& color)
 {
-    m_color = color;
+	m_color = color;
 }
 
 void Node::setLongitude(double longitude)
 {
-    m_longitude = longitude;
+	m_longitude = longitude;
 }
 
 void Node::setLatitude(double latitude)
 {
-    m_latitude = latitude;
+	m_latitude = latitude;
 }
 
 void Node::setGeoCoord(double longitude, double latitude)
 {
-    m_longitude = longitude;
-    m_latitude = latitude;
+	m_longitude = longitude;
+	m_latitude = latitude;
+}
+
+void Node::setName(const std::string& name)
+{
+	m_name = name;
 }
 
 int Node::getIndex() const
 {
-    return m_index;
+	return m_index;
 }
 
 QPoint Node::getCoord() const
 {
-    return m_coord;
+	return m_coord;
 }
 
 int Node::getX() const
 {
-    return m_coord.x();
+	return m_coord.x();
 }
 
 int Node::getY() const
 {
-    return m_coord.y();
+	return m_coord.y();
 }
 
 int Node::getRow() const
 {
-    return m_row;
+	return m_row;
 }
 
 int Node::getColumn() const
 {
-    return m_column;
+	return m_column;
 }
 
 QColor Node::getColor() const
 {
-    return m_color;
+	return m_color;
 }
 
 double Node::getLongitude() const
 {
-    return m_longitude;
+	return m_longitude;
 }
 
 double Node::getLatitude() const
 {
-    return m_latitude;
+	return m_latitude;
+}
+
+const std::string& Node::getName() const
+{
+	return m_name;
 }
--- GraphFramework/Node/node.h
+++ GraphFramework/Node/node.h
@@ -3,39 +3,46 @@
 
 #include <QPoint>
 #include <QColor>
+#include <string>
 
 class Node
 {
-    int m_index;
-    QPoint m_coord;
-    int m_row;
-    int m_column;
-    QColor m_color;
-    double m_longitude;
-    double m_latitude;
+	int m_index;
+	QPoint m_coord;
+	int m_row;
+	int m_column;
+	QColor m_color;
+	double m_longitude;
+	double m_latitude;
+	std::string m_name;
 
 public:
-    Node(int index = 0, QPoint p = QPoint(0, 0));
+	Node(
+		int index = 0,
+		QPoint p = QPoint(0, 0)
+	);
 
-    void setIndex(int index);
-    void setCoord(const QPoint& p);
-    void setGridPos(int row, int column);
-    void setRow(int row);
-    void setColumn(int column);
-    void setColor(const QColor& color);
-    void setLongitude(double longitude);
-    void setLatitude(double latitude);
-    void setGeoCoord(double longitude, double latitude);
+	void setIndex(int index);
+	void setCoord(const QPoint& p);
+	void setGridPos(int row, int column);
+	void setRow(int row);
+	void setColumn(int column);
+	void setColor(const QColor& color);
+	void setLongitude(double longitude);
+	void setLatitude(double latitude);
+	void setGeoCoord(double longitude, double latitude);
+	void setName(const std::string& name);
 
-    int getIndex() const;
-    QPoint getCoord() const;
-    int getX() const;
-    int getY() const;
-    int getRow() const;
-    int getColumn() const;
-    QColor getColor() const;
-    double getLongitude() const;
-    double getLatitude() const;
+	int getIndex() const;
+	QPoint getCoord() const;
+	int getX() const;
+	int getY() const;
+	int getRow() const;
+	int getColumn() const;
+	QColor getColor() const;
+	double getLongitude() const;
+	double getLatitude() const;
+	const std::string& getName() const;
 };
 
 #endif
